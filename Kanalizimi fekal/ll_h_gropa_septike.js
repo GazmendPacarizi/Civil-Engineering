34e2554d226ba965e5500e517582b5b59781dcd9
GaziPacarizi <gazmendpaqarizi@gmail.com>
2024-05-05 16:18:51 +0200

    perditesim i gropes septike

--- Kanalizimi fekal/ll_h_gropa_septike.js
+++ Kanalizimi fekal/ll_h_gropa_septike.js
@@ -1,49 +1,50 @@
 document.getElementById("llog_gropa_septike").onclick = function() {
+    // Parse input values
     let N0 = parseFloat(document.getElementById("Nr_banoreve").value);
-    N0 = Number(N0);
     let Pp = parseFloat(document.getElementById("Periudha_projektuese").value);
-    Pp = Number(Pp);
     let Pps = parseFloat(document.getElementById("Periudha_pastrimit").value);
     let q = parseFloat(document.getElementById("kerkesa_uje_per_person").value);
-    q = Number(q);
     let H = parseFloat(document.getElementById("Lartësia").value);
     let Hl = parseFloat(document.getElementById("Kufiri_lirë").value);
     let Ly = parseFloat(document.getElementById("Lymi").value);
     let Km = parseFloat(document.getElementById("koha_mbajtjes").value);
-    let Rrp = parseFloat(document.get("rritja_popullsise").value);
+    let Rrp = parseFloat(document.getElementById("rritja_popullsise").value);
 
-    let Nb = N0 * (1 + (Rrp / 100))**Pp;
-    //Prurja furnizuese
-    
+    // Numri i popullsise
+    let Nb = N0 * Math.pow((1 + Rrp / 100), Pp);
+
+    // Furnizimi me uje
     let Qf = q * Nb;
-    Qf = Number(Qsh);
 
-    //Prurja shkarkuese 80%
-
+    // Prurja shkarkuese
     let Qsh = 0.8 * Qf;
-    
 
-    //Vellimi i ujit
+    // Vellimi i ujit
+    let Vu = Qsh * Km / 24;
 
-    let Vu = qsh * Km / 24;
-
-    //Vellimi i lymit
-
+    // Vellimi i lymit
     let Vl = (Ly / 1000) * Nb * (Pps / 1);
 
-    //Vellimi total i gropes septike
-
+    // Vellimi total
     let V1 = Vu + Vl;
 
-    let A = (Vtot / H);
+    // Siperfaqja e gropes septike
+    let A = V1 / H;
 
-    let W = (A / 2)**0.5;
+    // Gjeresia e gropes septike
+    let W = Math.sqrt(A / 2);
 
+    // Gjatesia e gropes septike, ne rastin tone ne raport 2 me 1
     let L = 2 * W;
 
+    // Lartesia totale e gropes septike
     let Ht = H + Hl;
-    Ht = Number(Ht);
 
-    document.getElementById("rez1").innerHTML = L.toFixed(2);
-    document.getElementById("gjer_gseptike").innerHTML = W.toFixed(2);
-}   //document.getElementById("lar_gseptike").innerHTML = Ht.toFixed(2);
+    // Update HTML elements with results
+    document.getElementById("rez1").innerHTML = "Gjatësia e gropës septike është " + L.toFixed(2);
+    document.getElementById("rez2").innerHTML = W.toFixed(2);
+    document.getElementById("rez3").innerHTML = Ht.toFixed(2);
+};
+
+
+   
